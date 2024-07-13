5682c4825c4c81f192fa4556790c01cc3197487e
Abdelkabir <abdelkabir.ouadoukou@gmail.com>
2024-07-13 10:26:11 +0100

    fix: Cache

--- app/api/payments/route.js
+++ app/api/payments/route.js
@@ -1,20 +1,12 @@
 import clientPromise from "@/utils/mongodb";
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
+export async function GET() {
   try {
     const client = await clientPromise;
     const db = client.db(process.env.MONGODB_DB);
 
     const payments = await db.collection("payments").find({}).toArray();
-
-    // Add cache control headers to prevent caching
-    req.setHeader(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate"
-    );
-    req.setHeader("Pragma", "no-cache");
-    req.setHeader("Expires", "0");
 
     return NextResponse.json({ payments }, { status: 200 });
   } catch (error) {
@@ -22,20 +14,3 @@ export async function GET(req) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
 }
-
-// import clientPromise from "@/utils/mongodb";
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   try {
-//     const client = await clientPromise;
-//     const db = client.db(process.env.MONGODB_DB);
-
-//     const payments = await db.collection("payments").find({}).toArray();
-
-//     return NextResponse.json({ payments }, { status: 200 });
-//   } catch (error) {
-//     console.log(error);
-//     return NextResponse.json({ message: "Error", error }, { status: 500 });
-//   }
-// }
