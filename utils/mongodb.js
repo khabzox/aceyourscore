c60b3ff374fad80568ae5609fe8cb1dbb927ef24
Abdelkabir <abdelkabir.ouadoukou@gmail.com>
2024-07-13 10:35:33 +0100

    fix: Cache

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,59 +1,25 @@
+// utils/mongodb.js
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 const options = {};
 
+let client;
 let clientPromise;
 
-if (!uri) {
-  throw new Error("Please add your MongoDB URI to .env.local");
-}
-
-const client = new MongoClient(uri, options);
-
-async function connectMongoClient() {
-  try {
-    await client.connect();
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw new Error("MongoDB connection error");
-  }
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env.local");
 }
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
-    global._mongoClientPromise = connectMongoClient();
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  clientPromise = connectMongoClient();
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
 }
 
 export default clientPromise;
-
-// // utils/mongodb.js
-// import { MongoClient } from "mongodb";
-
-// const uri = process.env.MONGODB_URI;
-// const options = {};
-
-// let client;
-// let clientPromise;
-
-// if (!process.env.MONGODB_URI) {
-//   throw new Error("Please add your Mongo URI to .env.local");
-// }
-
-// if (process.env.NODE_ENV === "development") {
-//   if (!global._mongoClientPromise) {
-//     client = new MongoClient(uri, options);
-//     global._mongoClientPromise = client.connect();
-//   }
-//   clientPromise = global._mongoClientPromise;
-// } else {
-//   client = new MongoClient(uri, options);
-//   clientPromise = client.connect();
-// }
-
-// export default clientPromise;
