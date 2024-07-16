fe834e41a83f276f9409558c9b511b707c1d00f4
Abdelkabir <abdelkabir.ouadoukou@gmail.com>
2024-07-16 09:32:46 +0100

    build: text editor for creat articles

--- components/blog/admin/RichTextEditor.jsx
+++ components/blog/admin/RichTextEditor.jsx
@@ -1,29 +1,51 @@
-import React, { useMemo } from 'react';
-import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css';
+import React, { useMemo } from "react";
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
 
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 export default function RichTextEditor({ value, onChange }) {
-  const modules = useMemo(() => ({
-    toolbar: [
-      [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-      [{ size: [] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-      ['link', 'image', 'video'],
-      [{ 'color': [] }, { 'background': [] }],
-      ['clean']
-    ],
-  }), []);
+  const modules = useMemo(
+    () => ({
+      toolbar: [
+        [{ header: "1" }, { header: "2" }, { font: [] }],
+        [{ size: [] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { indent: "-1" },
+          { indent: "+1" },
+        ],
+        ["link", "image", "video"],
+        [{ color: [] }, { background: [] }],
+        ["clean"],
+      ],
+    }),
+    []
+  );
 
-  const formats = useMemo(() => [
-    'header', 'font', 'size',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image', 'video',
-    'color', 'background'
-  ], []);
+  const formats = useMemo(
+    () => [
+      "header",
+      "font",
+      "size",
+      "bold",
+      "italic",
+      "underline",
+      "strike",
+      "blockquote",
+      "list",
+      "bullet",
+      "indent",
+      "link",
+      "image",
+      "video",
+      "color",
+      "background",
+    ],
+    []
+  );
 
   return (
     <ReactQuill
@@ -33,7 +55,7 @@ export default function RichTextEditor({ value, onChange }) {
       formats={formats}
       theme="snow"
       className="mb-20"
-      style={{ height: '400px' }}
+      style={{ height: "400px" }}
     />
   );
 }
